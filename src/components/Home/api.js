8967d62a0531190f248809b9c405e773c9fdc675
vibin chemmannoor <vibinchemmannoor@gmail.com>
2025-11-15 15:22:28 +0530

    apapi file

---
+++ src/components/Home/api.js
@@ -0,0 +1,5 @@
+export const countyData = async () =>{
+    const response = await fetch("https://restcountries.com/v2/all?fields=name,region,flag")
+    if(!response.ok) throw new Error ('Failed to fetch countries');
+    return response.json()
+};
